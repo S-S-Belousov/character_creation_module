cc60af26ec86db2305549199b70775d8e209d4d2
Сергей Белоусов <ss.belousov@yandex.ru>
2022-12-27 14:07:53 +0300

    upgrade code

--- 1.py
+++ 1.py
@@ -1,24 +1,4 @@
-from math import sqrt
+def avg(first_num, second_num):
+    return first_num + second_num / 2
 
-message = ('Добро пожаловать в самую лучшую программу для вычисления '
-           'квадратного корня из заданного числа')
-print(message)
-
-
-def calculate_square_root(number):
-    """Вычисляет квадратный корень."""
-    return sqrt(number)
-
-
-def calc(your_number):
-    """Выводит результат на печать."""
-    if your_number <= 0:
-        return
-    result = calculate_square_root(your_number)
-    print(
-        'Мы вычислили квадратный корень из введённого вами числа.'
-        f' Это будет: {result}')
-
-
-print(message)
-calc(25.5)
+print(avg(5, 10))
